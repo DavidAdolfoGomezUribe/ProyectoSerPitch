cdf3cb26f66f1bdd459e62d8a6436ccd4a54491b
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-05-01 08:25:49 -0500

    feat: :sparkles: Add Ejemplo component and Page for testing layout

---
+++ src/pages/pruebas/ejemplo.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from "react"
+
+export default function Ejemplo() {
+    return (
+        <>
+            <h1 style={{ fontSize: "50px" }}>Esto es un título de prueba</h1>
+        </>
+    )
+}
---
+++ src/pages/pruebas/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Ejemplo from "./ejemplo"
+
+
+export default function Page (){
+    return(
+        <React.Fragment>
+            {/* aqui pueden insertar los componestes para previsualisar como se verian en la pagina */}
+            <Ejemplo />
+
+        </React.Fragment>
+    )
+}
